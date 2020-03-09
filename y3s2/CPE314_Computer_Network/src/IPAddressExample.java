68795c16b951a2039b143df1f0c7ac5258bcc659
natthawat <natthawat.tungruethaipak@mail.kmutt.ac.th>
2020-03-09 22:01:34 +0700

    get ip address in java network

--- y3s2/CPE314_Computer_Network/src/IPAddressExample.java
+++ y3s2/CPE314_Computer_Network/src/IPAddressExample.java
@@ -1,22 +1,23 @@
 import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.util.Enumeration;
 
 class IPAddressExample
 {
 	public static void main(String args[]) throws Exception
 	{
-		Enumeration e = NetworkInterface.getNetworkInterfaces();
-		while (e.hasMoreElements())
-		{
-			NetworkInterface n = (NetworkInterface) e.nextElement();
-			System.out.println(n);
-			Enumeration ee = n.getInetAddresses();
-			while (ee.hasMoreElements())
-			{
-				InetAddress i = (InetAddress) ee.nextElement();
-				System.out.println(i.getHostAddress());
-			}
-		}
+		InetAddress inetAddress = InetAddress.getLocalHost();
+		System.out.println("IP Address:- " + inetAddress.getHostAddress());
+		System.out.println("Host Name:- " + inetAddress.getHostName());
+//		Enumeration e = NetworkInterface.getNetworkInterfaces();
+//		while (e.hasMoreElements())
+//		{
+//			NetworkInterface n = (NetworkInterface) e.nextElement();
+//			System.out.println(n);
+//			Enumeration ee = n.getInetAddresses();
+//			while (ee.hasMoreElements())
+//			{
+//				InetAddress i = (InetAddress) ee.nextElement();
+//				System.out.println(i.getHostAddress());
+//			}
+//		}
 	}
 }
